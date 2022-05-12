7ab59ca15861fc16e3f05b8e801da723fb2ce1b1
Widyarso Joko Purnomo <widyjp10@gmail.com>
2022-05-12 22:04:47 +0700

    add drawer navigation

--- lib/widgets/page_scaffold.dart
+++ lib/widgets/page_scaffold.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:gita_gitroops/about_gita/view/about_gita_view.dart';
+import 'package:gita_gitroops/about_gitroops/view/about_gitroops_view.dart';
+import 'package:gita_gitroops/home/view/home_view.dart';
+import 'package:gita_gitroops/schedule/view/schedule_view.dart';
 import 'package:gita_gitroops/widgets/header.dart';
 import 'package:gita_gitroops/widgets/responsive_widget.dart';
 
@@ -17,7 +21,36 @@ class PageScaffold extends StatelessWidget {
             )
           : null,
       drawer: ResponsiveWidget.getScreenSizeId(context) < ResponsiveWidget.large
-          ? const Drawer()
+          ? Drawer(
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text('Home'),
+                    onTap: () {
+                      Navigator.pushNamed(context, HomeView.routeName);
+                    },
+                  ),
+                  ListTile(
+                    title: Text('Schedule'),
+                    onTap: () {
+                      Navigator.pushNamed(context, ScheduleView.routeName);
+                    },
+                  ),
+                  ListTile(
+                    title: Text('About Gita'),
+                    onTap: () {
+                      Navigator.pushNamed(context, AboutGitaView.routeName);
+                    },
+                  ),
+                  ListTile(
+                    title: Text('About Us'),
+                    onTap: () {
+                      Navigator.pushNamed(context, AboutGitroopsView.routeName);
+                    },
+                  ),
+                ],
+              ),
+            )
           : null,
       body: SingleChildScrollView(
         child: Align(
