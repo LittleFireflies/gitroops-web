fc90b4fd48bf3a978d1b56f9f800b6eaa4ae97dd
Widyarso Joko Purnomo <widyjp10@gmail.com>
2022-06-26 12:41:32 +0700

    fix web config

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Gitroops #TheNewTroops',
       theme: GitroopsTheme.mainTheme(),
       initialRoute: HomePage.routeName,
       routes: {
