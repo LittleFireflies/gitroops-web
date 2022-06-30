4a3068dae8f89d74ecb0a1e7194db11d23f2db92
Widyarso Joko Purnomo <widyjp10@gmail.com>
2022-06-30 08:12:17 +0700

    fix jiko

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,7 +3,7 @@ class AppConstants {
   static const twitterGitroops = 'https://twitter.com/gitroops';
   static const instagramGitroops = 'https://www.instagram.com/gitroops/';
   static const jikoshoukai =
-      '"Diam bukan berarti tidak memperhatikanmu, \naku Gita!"';
+      '"Diam bukan berarti tidak memperhatikanmu, aku Gita!"';
   static const aboutGita =
       '''Gita Sekar Andarini atau yang kerap disapa Gita ini adalah salah satu anggota JKT48 generasi 6. Gadis yang lahir pada 30 Juni 2001 ini pertama kali diperkenalkan ke fans pada 8 April 2018 di teater JKT48. Sebagai siswi Academy Class B, Gita harus mengulang dua kali ujian kenaikan kelas tahap pertama, yakni pada 25 Mei 2018 dan 7 Juni 2018, sebelum akhirnya dinyatakan lulus ujian kenaikan kelas tahap pertama pada 11 Agustus 2018. Keesokan harinya, 12 Agustus 2018, Gita lulus dari ujian kenaikan kelas tahap kedua, menjadikannya anggota generasi keenam terakhir yang dipromosikan sebagai siswi JKT48 Akademi Kelas A.
       
